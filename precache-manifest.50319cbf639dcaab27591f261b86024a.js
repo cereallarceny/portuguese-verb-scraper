1024e18c0d7bad072971a6c4c0c1feb20e17e3a1
Patrick Cason <me@patrickcason.com>
2020-02-10 18:47:11 +0000

    Updates

---
+++ precache-manifest.50319cbf639dcaab27591f261b86024a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bfd6c5fc22668d924733e9546bee727",
+    "url": "/portuguese-verb-scraper/index.html"
+  },
+  {
+    "revision": "8c531e9cadfc50583cb9",
+    "url": "/portuguese-verb-scraper/static/css/2.c0851dc3.chunk.css"
+  },
+  {
+    "revision": "8c531e9cadfc50583cb9",
+    "url": "/portuguese-verb-scraper/static/js/2.6b2c18ce.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portuguese-verb-scraper/static/js/2.6b2c18ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16c911cdce28283f6fc7",
+    "url": "/portuguese-verb-scraper/static/js/main.ba90dde2.chunk.js"
+  },
+  {
+    "revision": "4d5035424b5c820393cc",
+    "url": "/portuguese-verb-scraper/static/js/runtime-main.b78ae16d.js"
+  }
+]);
